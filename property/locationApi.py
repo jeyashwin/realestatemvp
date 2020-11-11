166dc035d8fae94ced6e37a56c233b5b215fb8af
Aravindhan Sundaram <aravindhansundram13@gmail.com>
2020-11-11 21:39:59 +0530

    services testcase finished

--- property/locationApi.py
+++ property/locationApi.py
@@ -4,7 +4,7 @@ from django.conf import settings
 gmapsClient = googlemaps.Client(key=settings.GOOGLE_MAPS_API_KEY)
 
 
-logging.basicConfig(level=logging.DEBUG, filename='completeApp.log', format='%(asctime)s - %(name)s - %(process)d - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+logging.basicConfig(level=logging.ERROR, filename='completeApp.log', format='%(asctime)s - %(name)s - %(process)d - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 logging = logging.getLogger(name='locationApiLogger')
 
 def get_lat_long_from_address(address):
