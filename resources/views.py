ac076d4c77f2d44ed44dc422d64b0427548fb89d
Aravindhan Sundaram <aravindhansundram13@gmail.com>
2020-12-16 22:01:53 +0530

    completed all changes in backend

--- resources/views.py
+++ resources/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
 
-
 from users.views import AuthenticationForm, LandlordSignupForm, StudentSignupForm, ForgotPasswordForm
 # Create your views here.
 
 def home(request):
-    value = signing.dumps('Aravindhan@123', key=settings.SECRET_KEY)
-    print(value)
-    print(signing.loads(value))
     context = {
         "form" : AuthenticationForm(),
         "LandlordSignupForm" : LandlordSignupForm(label_suffix=''),
